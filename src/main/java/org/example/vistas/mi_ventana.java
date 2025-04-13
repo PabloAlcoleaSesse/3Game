831fe088652300ae4961e9a63d85997b0f8a5e81
Pablo Alcolea Sesse <128633352+PabloAlcoleaSesse@users.noreply.github.com>
2025-04-13 22:08:55 +0200

    Creacion de la primera vista de la app con JFrame y swing

--- src/main/java/org/example/vistas/mi_ventana.java
+++ src/main/java/org/example/vistas/mi_ventana.java
@@ -1,4 +1,28 @@
 package org.example.vistas;
 
-public class mi_ventana {
+import javax.swing.*;
+import java.awt.*;
+
+public class mi_ventana extends javax.swing.JFrame {
+    public mi_ventana() {
+        setTitle("Inicio");
+        setSize(800, 600);
+        setMinimumSize(new java.awt.Dimension(600, 500));
+        
+
+
+        JLabel title = new JLabel("¿A que juego quieres jugar?", SwingConstants.CENTER);
+        title.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 24));
+        add(title, java.awt.BorderLayout.CENTER);
+
+        JButton boton_caballo = new JButton("Caballo");
+        boton_caballo.setFont(new java.awt.Font("Arial", java.awt.Font.PLAIN, 18));
+        boton_caballo.setPreferredSize(new Dimension(150, 50)); // Adjusted size
+
+        // Wrap the button in a JPanel with FlowLayout
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        buttonPanel.add(boton_caballo);
+
+        add(buttonPanel, java.awt.BorderLayout.WEST); // Add the panel instead of the button
+    }
 }
